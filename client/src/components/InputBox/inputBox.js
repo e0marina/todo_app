d2a048618a465542d358dc9a28c0a9107f4720dc
Emily M <30880308+e0marina@users.noreply.github.com>
2020-12-09 12:16:36 -0800

    imported font awesome empty cirlce

--- client/src/components/InputBox/inputBox.js
+++ client/src/components/InputBox/inputBox.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Form, Input } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle as regularCircle } from "@fortawesome/free-regular-svg-icons";
 import "./inputBox.css";
 
 const InputBox = (props) => {
   return (
     <Form>
+      <FontAwesomeIcon icon={regularCircle} />
       <Input id="inputBox" placeholder="create a new todo" />
     </Form>
   );
