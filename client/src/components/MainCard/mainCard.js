1b7e785f3b46d7195c7832062039741cdb342464
Emily M <30880308+e0marina@users.noreply.github.com>
2021-01-16 10:20:15 -0800

    adding main card

---
+++ client/src/components/MainCard/mainCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./mainCard.css";
+
+const MainCard = (props) => {
+  return (
+    <div className="row">
+      <div className="col-6">
+        <div className="card">
+          <div className="card-body">This is some text within a card body.</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MainCard;
