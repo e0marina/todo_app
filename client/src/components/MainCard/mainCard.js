71fc5e102b60ea3dd8147ede0a56a7cfa0ae65b6
Emily M <30880308+e0marina@users.noreply.github.com>
2021-01-19 15:32:02 -0800

    updating InputBox and Jumbotron

--- client/src/components/MainCard/mainCard.js
+++ client/src/components/MainCard/mainCard.js
@@ -1,12 +1,35 @@
 import React from "react";
+import InputBox from "../InputBox/inputBox.js";
+import moon from "../images/icon-moon.svg";
+
 import "./mainCard.css";
 
 const MainCard = (props) => {
   return (
-    <div className="row">
-      <div className="col-3">
-        <div className="card">
-          <div className="card-body">card body</div>
+    <div>
+      <div className="container" id="main-wrapper">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <div id="heading">TODO</div>
+              </div>
+
+              <div className="col">
+                <img src={moon} alt="moon icon" id="moon" />
+              </div>
+            </div>
+            <div className="row">
+              <InputBox />
+            </div>
+            <div className="card">
+              <div className="card-body">
+                <InputBox />
+                <InputBox />
+                <InputBox />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
